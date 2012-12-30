9f416572e523a1121b32558dff3fc5e9156a10b6
dhsu <dhsu@integrity-apps.com>
2012-12-29 19:25:16 -0800

    add problem 23

---
+++ src/main/scala/org/krayne/euler/from020/Problem023.scala
@@ -0,0 +1,19 @@
+package org.krayne.euler.from020
+
+import org.krayne.euler.Problem
+
+object Problem023 extends Problem[Int] {
+  override val value: Int = {
+    val abundants = abundantsLessThan(28123 + 1)
+    (1 to 28123).filterNot(isSumFrom(_, abundants)).sum
+  }
+  
+  private def properDivisorSum(x: Int): Int = (1 to x/2).filter(x % _ == 0).sum
+  private def isAbundant(x: Int): Boolean = properDivisorSum(x) > x
+  private def abundantsLessThan(x: Int): Set[Int] = (1 until x).filter(isAbundant).toSet
+  private def isSumFrom(x: Int, numbers: Set[Int]): Boolean = numbers.find(n => numbers.contains(x - n)).isDefined
+  
+  def main(args: Array[String]) {
+    println(Problem023)
+  }
+}
