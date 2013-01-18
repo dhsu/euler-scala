91d325345352ddeeacb042f039894f60cafe4b5d
dhsu <dhsu@integrity-apps.com>
2013-01-18 10:13:53 -0800

    fix test for problem 40

--- src/test/scala/org/krayne/euler/From040.scala
+++ src/test/scala/org/krayne/euler/From040.scala
@@ -4,6 +4,6 @@ import org.scalatest.FunSuite
 import org.krayne.euler.from040._
 
 class From040 extends FunSuite {
-  test("Problem 40") {assert(Problem040.value == 443839)}
+  test("Problem 40") {assert(Problem040.value == 210)}
   test("Problem 41") {assert(Problem041.value == 7652413L)}
 }
